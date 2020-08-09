1435dd4bfa6f918ba8f616ccce9e64e627729980
Vinita V Nayak <vinitha.nayak1994@gmail.com>
2020-08-09 14:33:32 +0530

    Controller, yaml files added

--- proman-api/target/generated-sources/io/swagger/api/ApiException.java
+++ proman-api/target/generated-sources/io/swagger/api/ApiException.java
@@ -1,6 +1,6 @@
 package io.swagger.api;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-08-08T21:54:52.245+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-08-08T22:38:54.145+05:30")
 
 public class ApiException extends Exception{
     private int code;
